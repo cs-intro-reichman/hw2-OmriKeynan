ceb0dd8eb822e01979e4bd3a8702b921f8fc6c15
Omri Keynan <omri.keynan@post.runi.ac.il>
2024-11-16 19:57:31 +0200

    Completed Assignment

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,38 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
-	public static void main(String args[]) {
-	    // Replace this comment with your code
-	}
+    public static void main(String args[]) {
+        int seed = Integer.parseInt(args[0]);
+		String mode = (args[1]);
+		String verbose = "v";
+		String consice = "c";
+		if(mode.equals(verbose)) {
+			for (int i = 1; i <= seed; i++) {
+				int currentNum = i;
+				int steps = 0;
+				System.out.print(i + " "); 
+				
+				do {
+					if (currentNum != i) {
+						System.out.print(currentNum + " "); 
+					}
+					if (currentNum % 2 == 0) {
+						currentNum = currentNum / 2;
+					} else {
+						currentNum = (currentNum * 3) + 1;
+					}
+					steps++;
+				} while (currentNum != 1);
+				
+				System.out.print("1 ");
+				steps++;
+
+				System.out.print("(" + steps + ")");
+				System.out.println();
+			}
+			System.out.println("Every one of the first " + seed + " hailstone sequences reached 1.");
+		}
+		if (mode.equals(consice)){
+			System.out.println("Every one of the first " + seed + " hailstone sequences reached 1.");
+		} 
+    }
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,25 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+		// Does the same with the minutes part of the input.
+		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int minutesToAdd= Integer.parseInt(args[1]);
+        int totalMinutes = ((hours*60) + minutes + minutesToAdd);
+        int totalHours = totalMinutes / 60; 
+        int newHour = totalHours % 24;
+        int newMinutes = totalMinutes - (totalHours*60);
+        if (newMinutes - 10 < 0) {
+            System.out.println(newHour + ":" + "0" + newMinutes);
+        } 
+        else { 
+        System.out.println(newHour + ":" + newMinutes);
+        }
+        // System.out.println("hours: " + hours);
+        // System.out.println("minutes: " + minutes);
+        // System.out.println("minutes to add: " + minutesToAdd);
+        // System.out.println("New hours: " + newHour);
+        // System.out.println("New minutes: " + newMinutes);
+
     }
+    
 }
